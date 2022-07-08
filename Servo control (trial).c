b3c5e0b80114a3e84064984235c9597fc0db0904
A_Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-07 21:53:06 -0700

    Update and rename Servo control.c to Servo control (trial).c

---
+++ Servo control (trial).c
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
+int joyX = 0;
+int joyY = 1;
+int joyVal;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(5);
+  servo2.attach(7);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+joyVal = analogRead(joyX);
+joyVal = map(joyVal, 0, 1023, 0, 180);
+servo1.write(joyVal);
+delay(12);
+}
+
